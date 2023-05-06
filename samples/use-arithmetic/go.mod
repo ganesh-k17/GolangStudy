b8d519d5943bbe07a3df36c4ddd611938fddcd89
sskumargan <ganku007@live.in>
2023-05-06 14:00:35 +0530

    added a folder which uses package which i have released from another repo

---
+++ samples/use-arithmetic/go.mod
@@ -0,0 +1,5 @@
+module github.com/mySamples
+
+go 1.16
+
+require github.com/ganesh-k17/go-arithimetic v1.0.3 // indirect
---
+++ samples/use-arithmetic/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	// "github.com/arithmetic/addition"
+	"github.com/ganesh-k17/go-arithimetic/addition"
+	"github.com/pborman/uuid"
+)
+
+func main() {
+	uuid := uuid.NewRandom()
+	fmt.Println("addition - ", addition.Add(5, 6))
+	fmt.Println("uuid - ", uuid)
+	fmt.Println("end")
+}
